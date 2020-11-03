d9f196d634bd30e5d8cf19066c6b6dbaeda33056
Gregor Hermens <gregor.hermens@a-mazing.de>
2020-11-03 11:29:22 +0100

    [FEATURE] prevent clicks on links inside the placeholder from loading the video

--- Resources/Public/JavaScript/media2click.js
+++ Resources/Public/JavaScript/media2click.js
@@ -1,16 +1,22 @@
-document.onreadystatechange = function() {
-    if (document.readyState === 'complete') {
-        let placeholderList = document.querySelectorAll('.media2click-placeholder');
-        placeholderList.forEach(function(placeholder) {
-            let mediaFrame = placeholder.nextElementSibling;
-            if (mediaFrame.nodeName === 'IFRAME') {
-                placeholder.addEventListener('click', function(event) {
-                    mediaFrame.setAttribute('src', mediaFrame.getAttribute('data-src'));
-                    mediaFrame.setAttribute('style', 'display: block;');
-                    placeholder.setAttribute('style', 'display: none;');
-                    event.preventDefault();
-                }, false);
-            }
-        });
-    }
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    let placeholderAnchorList = document.querySelectorAll('.media2click-placeholder a');
+    placeholderAnchorList.forEach(function (anchor) {
+      anchor.addEventListener('click', function (event) {
+        event.stopPropagation();
+      }, false);
+    });
+    let placeholderList = document.querySelectorAll('.media2click-placeholder');
+    placeholderList.forEach(function (placeholder) {
+      let mediaFrame = placeholder.nextElementSibling;
+      if (mediaFrame.nodeName === 'IFRAME') {
+        placeholder.addEventListener('click', function (event) {
+          mediaFrame.setAttribute('src', mediaFrame.getAttribute('data-src'));
+          mediaFrame.setAttribute('style', 'display: block;');
+          placeholder.setAttribute('style', 'display: none;');
+          event.preventDefault();
+        }, false);
+      }
+    });
+  }
 };
