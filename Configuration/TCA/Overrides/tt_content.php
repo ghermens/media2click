da1c85311c3d97dee5da0ce2a6be7e4ae5600633
Gregor Hermens <gregor.hermens@a-mazing.de>
2021-05-03 12:06:14 +0200

    [TASK] add softref to TCA url field

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -14,6 +14,7 @@ defined('TYPO3_MODE') || die;
                     'allowLanguageSynchronization' => true,
                 ],
                 'eval' => 'trim,Amazing\\Media2click\\Evaluation\\UrlSchemeEvaluation,required',
+                'softref' => 'typolink',
                 'fieldControl' => [
                     'linkPopup' => [
                         'options' => [
