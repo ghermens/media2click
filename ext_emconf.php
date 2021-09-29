7d0d5b411ba5d2884c9ba8d83d730680ec26f3c7
Gregor Hermens <gregor.hermens@a-mazing.de>
2021-09-29 17:32:16 +0200

    [TASK] update version constraints for TYPO3 11

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '10.4.0-11.3.99',
+                    'typo3' => '10.4.0-11.5.99',
                 ],
             'conflicts' =>
                 [
