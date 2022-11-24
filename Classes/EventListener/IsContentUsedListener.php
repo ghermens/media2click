d1ae66a36605d058299fab57114fcbf5b70e7def
Gregor Hermens <gregor.hermens@a-mazing.de>
2022-11-24 17:07:38 +0100

    [TASK] replace hook with PSR-14 event

---
+++ Classes/EventListener/IsContentUsedListener.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Amazing\Media2click\EventListener;
+
+use TYPO3\CMS\Backend\View\Event\IsContentUsedOnPageLayoutEvent;
+
+final class IsContentUsedListener
+{
+    public function __invoke(IsContentUsedOnPageLayoutEvent $event): void
+    {
+        $record = $event->getRecord();
+        if ($record['tx_media2click_parentid'] > 0) {
+            if ($record['colPos'] === 1962587) {
+                $event->setUsed(true);
+            }
+        }
+    }
+}
