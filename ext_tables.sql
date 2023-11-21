c05bbc6d5e999af2e3b876273ff4204c49155d32
Gregor Hermens <gregor.hermens@a-mazing.de>
2023-11-21 09:55:17 +0100

    [TASK] Beautify ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,18 +1,18 @@
 CREATE TABLE tt_content
 (
-    tx_media2click_iframe_src VARCHAR(1024) DEFAULT '' NOT NULL,
-    tx_media2click_iframe_ratio VARCHAR(16) DEFAULT '' NOT NULL,
-    tx_media2click_content INT(11) DEFAULT '0' NOT NULL,
-    tx_media2click_parentid INT(11) DEFAULT '0' NOT NULL,
-		tx_media2click_host INT(11) DEFAULT '0' NOT NULL
+	tx_media2click_iframe_src   VARCHAR(1024) DEFAULT ''  NOT NULL,
+	tx_media2click_iframe_ratio VARCHAR(16)   DEFAULT ''  NOT NULL,
+	tx_media2click_content      INT(11)       DEFAULT '0' NOT NULL,
+	tx_media2click_parentid     INT(11)       DEFAULT '0' NOT NULL,
+	tx_media2click_host         INT(11)       DEFAULT '0' NOT NULL
 );
 
 CREATE TABLE tx_media2click_domain_model_host
 (
-    title VARCHAR(128) DEFAULT '' NOT NULL,
-    host VARCHAR(128) DEFAULT '' NOT NULL,
-    privacy_statement_link VARCHAR(1024) DEFAULT '' NOT NULL,
-    placeholder text,
-    allow_permanent int(3) DEFAULT '0' NOT NULL,
-    logo int(3) DEFAULT '0' NOT NULL
+	title                  VARCHAR(128)  DEFAULT ''  NOT NULL,
+	host                   VARCHAR(128)  DEFAULT ''  NOT NULL,
+	privacy_statement_link VARCHAR(1024) DEFAULT ''  NOT NULL,
+	placeholder            TEXT,
+	allow_permanent        INT(3)        DEFAULT '0' NOT NULL,
+	logo                   INT(3)        DEFAULT '0' NOT NULL
 );
