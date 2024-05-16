cfc79312ccbfaf4cdcab46281017b01d86e02458
Gregor Hermens <gregor.hermens@a-mazing.de>
2024-05-16 10:04:25 +0200

    [TASK] Remove deprecated hook

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,25 +10,21 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') || die;
 
-(function() {
-    /** @var RendererRegistry $rendererRegistry */
-    $rendererRegistry = GeneralUtility::makeInstance(RendererRegistry::class);
-    $rendererRegistry->registerRendererClass(YouTubeRenderer::class);
-    $rendererRegistry->registerRendererClass(VimeoRenderer::class);
+/** @var RendererRegistry $rendererRegistry */
+$rendererRegistry = GeneralUtility::makeInstance(RendererRegistry::class);
+$rendererRegistry->registerRendererClass(YouTubeRenderer::class);
+$rendererRegistry->registerRendererClass(VimeoRenderer::class);
 
-    ExtensionManagementUtility::addPageTSConfig(
-        '@import "EXT:media2click/Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig"'
-    );
+ExtensionManagementUtility::addPageTSConfig(
+    '@import "EXT:media2click/Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig"'
+);
 
-    ExtensionUtility::configurePlugin(
-        'Media2click',
-        'List',
-        [HostController::class => 'index'],
-        [],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
-    );
+ExtensionUtility::configurePlugin(
+    'Media2click',
+    'List',
+    [HostController::class => 'index'],
+    [],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Amazing\\Media2click\\Evaluation\\UrlSchemeEvaluation'] = '';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['record_is_used']['tx_media2click']
-        = 'Amazing\\Media2click\\Hooks\\UsedRecords->addMedia2ClickContent';
-}) ();
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Amazing\\Media2click\\Evaluation\\UrlSchemeEvaluation'] = '';
